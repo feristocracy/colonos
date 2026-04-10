45d0a287a6839faa448204f94c3bfade715015c5
Fernando Maldonado <fermaldonadosolis@gmail.com>
2026-04-09 18:02:13 -0600

    le cambiamos el texto por icono en colonos.show para ver si tienen recibo o no cargado

--- resources/views/colonos/show.blade.php
+++ resources/views/colonos/show.blade.php
@@ -179,15 +179,14 @@
                                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                                         ${{ number_format($pago->monto, 2) }}
                                                     </td>
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                                    <td class="flex items-center px-6 py-4 whitespace-nowrap text-sm">
                                                         @if($pago->recibo_path)
                                                             <a href="{{ asset('storage/' . $pago->recibo_path) }}"
-                                                               target="_blank"
-                                                               class="text-indigo-600 hover:text-indigo-800 font-medium">
-                                                                Ver recibo
+                                                               target="_blank">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg>
                                                             </a>
                                                         @else
-                                                            <span class="text-gray-400">Sin recibo</span>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="m840-234-80-80v-446H314l-80-80h526q33 0 56.5 23.5T840-760v526ZM792-56l-64-64H200q-33 0-56.5-23.5T120-200v-528l-64-64 56-56 736 736-56 56ZM240-280l120-160 90 120 33-44-283-283v447h447l-80-80H240Zm297-257ZM424-424Z"/></svg>
                                                         @endif
                                                     </td>                                                    
                                                 </tr>
